8b33880087e72fd6c5b13b09fc886893063912a5
Aylla Scaglia <aylla@scaglia.com.br>
2025-05-20 21:31:13 -0300

    :sparkles: Implementando a feature Tema

--- src/main/java/com/generation/blogPessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogPessoal/repository/PostagemRepository.java
@@ -1,9 +1,13 @@
 package com.generation.blogPessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.blogPessoal.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+
+	List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 
 }
