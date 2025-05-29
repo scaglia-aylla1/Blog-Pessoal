6a66ed3d8a11f9b00bb0f09a4d73630adb7503a4
Aylla Scaglia <aylla@scaglia.com.br>
2025-05-28 21:17:30 -0300

    :rocket: Ajustes de configuração para o Deploy

--- src/main/java/com/generation/blogPessoal/security/BasicSecurityConfig.java
+++ src/main/java/com/generation/blogPessoal/security/BasicSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -69,7 +70,8 @@ public class BasicSecurityConfig {
 	                .anyRequest().authenticated())
 	        .authenticationProvider(authenticationProvider())
 	        .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
-	        .httpBasic(withDefaults());
+	        .httpBasic(withDefaults())
+	        .formLogin(Customizer.withDefaults());
 
 		return http.build();
 
